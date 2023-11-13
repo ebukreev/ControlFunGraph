70285c0ab4cc7f1d23bf862a6f2ebe11a50b5050
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2023-11-13 23:34:39 +0300

    Add rider support

--- control-fun-graph-intellij/build.gradle.kts
+++ control-fun-graph-intellij/build.gradle.kts
@@ -1,47 +1,62 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.0"
-  id("org.jetbrains.intellij") version "1.15.0"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "ru.itmo"
 version = "1.0-SNAPSHOT"
 
+val ideType: String = System.getenv("cfg_ide_type") ?: "idea"
+
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2022.2.5")
-  type.set("IC") // Target IDE Platform
+    // https://www.jetbrains.com/intellij-repository/snapshots
+    // https://www.jetbrains.com/intellij-repository/releases
+    when (ideType) {
+        "rider" -> {
+            version.set("2023.2.3")
+            type.set("RD")
+        }
+        "idea" -> {
+            version.set("2023.2.5")
+            type.set("IC")
+        }
+        else -> error("unknown cfg_ide_type=$ideType")
+    }
+    this.downloadSources.set(true)
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+
+    plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
 
-  patchPluginXml {
-    sinceBuild.set("222")
-    untilBuild.set("232.*")
-  }
+    patchPluginXml {
+        sinceBuild.set("222")
+        untilBuild.set("232.*")
+    }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
