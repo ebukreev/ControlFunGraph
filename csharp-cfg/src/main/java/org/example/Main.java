35c0f4ebf44db6ba7676ceb16a23b7fb27782d4d
Sergey Fedorov <sergey_fedorov200001@mail.ru>
2023-11-14 21:37:53 +0300

    csharp cfg linux fix

--- csharp-cfg/src/main/java/org/example/Main.java
+++ csharp-cfg/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void createCsharpCfg(String inputCsharpCode) {
 
         try {
             String os = System.getProperty("os.name").toLowerCase();
@@ -16,50 +16,29 @@ public class Main {
             } else if (os.contains("nix") || os.contains("nux")) {
                 // TODO
                 // Linux
-                File executableFile = new File("csharp-cfg/src/main/java/org/example/net6.0/linux-x64/Hackathon");
-                csharpProgramPath = executableFile.getAbsolutePath();
-                //csharpProgramPath = "./csharp-cfg\\src\\main\\java\\org\\example\\net6.0\\linux-x64\\Hackathon";
+                var filePath = "net6.0/linux-x64/Hackathon";
+                var curFile = new File(filePath);
+
+                // Создаем процесс для выполнения chmod +x
+                Process chmodProcess = new ProcessBuilder("chmod", "+x", curFile.getAbsolutePath()).start();
+                chmodProcess.waitFor();
+
+                csharpProgramPath = curFile.getAbsolutePath();
+
             } else if (os.contains("mac")) {
                 // MacOS
-                csharpProgramPath = "./csharp-cfg\\src\\main\\java\\org\\example\\net6.0\\osx-x64\\Hackathon";
+                var filePath = "net6.0/osx-x64/Hackathon";
+                var curFile = new File(filePath);
+
+                // Создаем процесс для выполнения chmod +x
+                Process chmodProcess = new ProcessBuilder("chmod", "+x", curFile.getAbsolutePath()).start();
+                chmodProcess.waitFor();
+
+                csharpProgramPath = curFile.getAbsolutePath();
             } else {
                 System.out.println("Unsupported operating system");
                 return;
             }
-
-
-            // Передаем строку в программу на C#
-            String inputString =
-                    "private static int TestContinue(int x, int y)\n" +
-                            "{\n" +
-                            "    int c = 10;\n" +
-                            "    c++;\n" +
-                            "    int a = 5;\n" +
-                            "    String b = \"str\";\n" +
-                            "    for (int k = 0; k < a; k++)\n" +
-                            "    {\n" +
-                            "        for (int i = 1; i < c; i++)\n" +
-                            "        {\n" +
-                            "            if (c != a)\n" +
-                            "            {\n" +
-                            "                continue;\n" +
-                            "            }\n" +
-                            "             else\n" +
-                            "             {\n" +
-                            "                   if (c == a)\n" +
-                            "                   {\n" +
-                            "                       break;\n" +
-                            "                   }\n" +
-                            "                c++;\n" +
-                            "            }\n" +
-                            "            c--;\n" +
-                            "        }\n" +
-                            "    }\n" +
-                            "    int result = c;\n" +
-                            "    return result;\n" +
-                            "}\n" +
-                            "!!!END!!!";
-
 
             // Создаем процесс для выполнения программы на C#
             Process process = new ProcessBuilder(csharpProgramPath).start();
@@ -68,7 +47,7 @@ public class Main {
             OutputStream outputStream = process.getOutputStream();
             InputStream inputStream = process.getInputStream();
 
-            outputStream.write(inputString.getBytes());
+            outputStream.write(inputCsharpCode.getBytes());
             outputStream.flush();
             outputStream.close();
 
@@ -85,6 +64,43 @@ public class Main {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) {
+
+        // Передаем строку в программу на C#
+        String inputString =
+                "private static int TestContinue(int x, int y)\n" +
+                        "{\n" +
+                        "    int c = 10;\n" +
+                        "    c++;\n" +
+                        "    int a = 5;\n" +
+                        "    String b = \"str\";\n" +
+                        "    for (int k = 0; k < a; k++)\n" +
+                        "    {\n" +
+                        "        for (int i = 1; i < c; i++)\n" +
+                        "        {\n" +
+                        "            if (c != a)\n" +
+                        "            {\n" +
+                        "                continue;\n" +
+                        "            }\n" +
+                        "             else\n" +
+                        "             {\n" +
+                        "                   if (c == a)\n" +
+                        "                   {\n" +
+                        "                       break;\n" +
+                        "                   }\n" +
+                        "                c++;\n" +
+                        "            }\n" +
+                        "            c--;\n" +
+                        "        }\n" +
+                        "    }\n" +
+                        "    int result = c;\n" +
+                        "    return result;\n" +
+                        "}\n" +
+                        "!!!END!!!";
+
+        // Тестируем функцию
+        createCsharpCfg(inputString);
     }
 }
