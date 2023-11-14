ba8c2e8ebad2a64806c7d562a9f984655542a7d3
Evgeny Bukreev <ebukreev@ptsecurity.com>
2023-11-14 18:39:37 +0300

    fix kotlin cfg for java17

--- kotlin-cfg/src/main/kotlin/Entrypoint.kt
+++ kotlin-cfg/src/main/kotlin/Entrypoint.kt
@@ -1,8 +1,19 @@
 import guru.nidi.graphviz.engine.Format
 import kastree.ast.Node
 import kastree.ast.psi.Converter
-import kastree.ast.psi.Parser
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.MessageRenderer.PLAIN_RELATIVE_PATHS
+import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
+import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.com.intellij.psi.PsiErrorElement
+import org.jetbrains.kotlin.com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.com.intellij.testFramework.LightVirtualFile
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 object Entrypoint {
@@ -22,4 +33,37 @@ object Entrypoint {
         }
     }).parseFile(code)
 
+    private open class Parser(private val converter: Converter = Converter) {
+        private val configuration = CompilerConfiguration().apply {
+            put(
+                CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY,
+                PrintingMessageCollector(System.err, PLAIN_RELATIVE_PATHS, false)
+            )
+        }
+
+        private val proj by lazy {
+            KotlinCoreEnvironment.createForProduction(
+                Disposer.newDisposable(),
+                configuration,
+                EnvironmentConfigFiles.JVM_CONFIG_FILES
+            ).project
+        }
+
+        fun parseFile(code: String) = converter.convertFile(parsePsiFile(code))
+
+        fun parsePsiFile(code: String) =
+            PsiManager.getInstance(proj).findFile(LightVirtualFile("temp.kt", KotlinFileType.INSTANCE, code)) as KtFile
+
+        data class ParseError(
+            val file: KtFile,
+            val errors: List<PsiErrorElement>
+        ) : IllegalArgumentException("Failed with ${errors.size} errors, first: ${errors.first().errorDescription}")
+
+        companion object : Parser() {
+            init {
+                // To hide annoying warning on Windows
+                System.setProperty("idea.use.native.fs.for.win", "false")
+            }
+        }
+    }
 }
